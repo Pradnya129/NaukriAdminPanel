01a5ea07894e0648e831cd82272edf692efe3bcc
Pradnya129 <pradnyamandekar129@gmail.com>
2026-04-27 10:35:16 +0530

    config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  output: 'export',
+
+  basePath: '/NaukriAdminPanel',
+  assetPrefix: '/NaukriAdminPanel/',
+
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
